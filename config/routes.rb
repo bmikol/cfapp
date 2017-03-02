c2bff19a846cfe08017c92f0cdcf022aa7e20fc9
Brian Mikol <github@sffogworks.com>
2017-03-01 16:49:00 -0800

    5.1: Update routing in navigation, adding new product

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  get 'static_pages/about'
 
-  get 'static_pages/contact'
+  get 'about' =>					'static_pages#about'
+  get 'contact' =>				'static_pages#contact'
+  get 'landing_page' =>		'static_pages#landing_page'
 
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
