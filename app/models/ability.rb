48cae52e32fe7f66e70826b6fd78af637eefaef5
Brian Mikol <github@sffogworks.com>
2017-03-09 16:43:03 -0800

    5.10: Create Admin role and add comment moderation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # Guest user (not logged in)
-    can :manage, User, id: user.id # :manage matches any action on User, provided the user has the same ID as her/his user record
+    if user.admin?
+    	can :manage, :all
+    else
+	    can :manage, User, id: user.id # :manage matches any action on User, provided the user has the same ID as her/his user record
+    	can :read, :all
+    	can :create, Comment
+    end
   end
 end
